8878d3f4afb47e34ffb6a6ac755fd85b78c44b4b
bbh4897 <42186003+bbh4897@users.noreply.github.com>
2022-08-21 19:40:42 +0300

    init project

---
+++ demospringproject/src/main/java/com/brcubg/demoSpringProject/DemoSpringProjectApplication.java
@@ -0,0 +1,14 @@
+package com.brcubg.demoSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringProjectApplication {
+
+    public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(DemoSpringProjectApplication.class, args);
+    }
+
+}
